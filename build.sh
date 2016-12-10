0056d15c0b5e75c19c8c640e457f2accbdb81763
Alex von Renteln <alex@aeffle.de>
2016-12-10 22:07:20 +0100

    move run to build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,12 @@
 docker build -t build-img -f Dockerfile.build . 
 
 
-ID=$(docker create -it build-img bash)
+ID=$(docker create -it build-img)
 docker cp src ${ID}:/app
-docker start -i ${ID} mvn package
+docker start -i ${ID}
 docker cp ${ID}:/app/target/app.war ./target/app.war
+docker rm ${ID}
 
 # build container with app
-#docker build -t img .
+docker build -t img .
 
